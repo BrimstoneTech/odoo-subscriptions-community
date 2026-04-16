e2c91d76b5618eb58374abf15e25930b6b4695e8
BrimstoneTech <brimstonetech1@gmail.com>
2026-04-16 11:47:46 +0300

    style: completely restructure html description and fix all mojibake

--- flexirenew/__manifest__.py
+++ flexirenew/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Subscriptions: Ultimate Multi-Level Follow-Up & Analytics',
-    'version': '1.1.3',
+    'version': '1.1.4',
     'price': 294.00,
     'currency': 'USD',
     'license': 'OPL-1',
